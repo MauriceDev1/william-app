bb45a603e62094a44c87a6025d35b094a26501e3
MauriceDev1 <mrvolkwyn@gmail.com>
2021-03-01 22:18:59 +0200

    add code to git hub

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id:1,
+            name:'Redcap Fire Starter',
+            category:'Gel',
+            image:'/images/product-1.png',
+            price:120,
+            brand:'RedCap',
+            rating:4.5,
+            numReviews:10, 
+            descriptiom:''
+        },
+        {
+            _id:2,
+            name:'Redcap Quick Gel',
+            category:'Gel',
+            image:'/images/product-1.png',
+            price:200,
+            brand:'RedCap',
+            rating:5,
+            numReviews:10, 
+            descriptiom:''
+        },
+        {
+            _id:3,
+            name:'Redcap Gel',
+            category:'Gel',
+            image:'/images/product-1.png',
+            price:180,
+            brand:'RedCap',
+            rating:4,
+            numReviews:10, 
+            descriptiom:''
+        },
+        {
+            _id:4,
+            name:'Redcap Liquid',
+            category:'Gel',
+            image:'/images/product-1.png',
+            price:90,
+            brand:'RedCap',
+            rating:3,
+            numReviews:10, 
+            descriptiom:''
+        },
+        {
+            _id:5,
+            name:'Redcap Fire Liquid',
+            category:'Gel',
+            image:'/images/product-1.png',
+            price:50,
+            brand:'RedCap',
+            rating:2,
+            numReviews:10, 
+            descriptiom:''
+        },
+        {
+            _id:6,
+            name:'Redcap stoves',
+            category:'Equipment',
+            image:'/images/product-1.png',
+            price:500,
+            brand:'RedCap',
+            rating:2,
+            numReviews:4, 
+            descriptiom:''
+        }
+    ]
+}
+
+export default data;
